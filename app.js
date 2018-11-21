69dfa9dd1f76dec1b3fd6f9f9109654addc3c3aa
sunkhanna <sunaina.khanna01@gmail.com>
2018-11-21 15:30:06 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,10 +1,5 @@
-//const express = require('express')
-const sls = require('serverless-http')
-const bodyParser = require('body-parser')
 var AWS = require("aws-sdk");
-//const app = express()
 
-//app.use(bodyParser.json({ strict: false }));
 
 const USERS_TABLE = process.env.USERS_TABLE;
 const dynamoDb = new AWS.DynamoDB.DocumentClient();
@@ -21,7 +16,6 @@ module.exports.hello = (event,context,callback) => {
       message:'my first serverless api'
     }),
   };
-  console.log("response-------------->",response);
   callback(null,response);
 }
 
@@ -58,12 +52,7 @@ module.exports.getUsers = function(event, context, callback){
 }
 
 module.exports.createUser = function(event, context, callback){
-  console.log("event--------------->",event);
-	 console.log("event--------------->",event.body);	 
 	  var json = JSON.parse(event.body);
-  console.log("json----------------------->",json.userId);
-	 console.log("user id--------------->",event.body.userId);
-	 	 console.log("name--------------->",event.body.name);
   var param = {
     Item : {
       "userId":json.userId,
@@ -72,7 +61,6 @@ module.exports.createUser = function(event, context, callback){
     TableName :  USERS_TABLE
   };
   
-  console.log("oaram ---------------------------->>>>>>",param);
   
   dynamoDb.put(param, function(err, data){
 	   
@@ -105,68 +93,4 @@ module.exports.createUser = function(event, context, callback){
   });
 }
 
-// module.exports.getData = (event,context,callback) => {
 
-// }
-// app.get('/', async (req, res, next) => {
-//       var res = {
-//         'statusCode': 200,
-//         'headers': { 'Content-Type': 'application/json' },
-//         'body': JSON.stringify({ 'username': 'bob', 'id': 20 })
-//     }
-//       callback(null,res);
-//     //  res.send();
-//   //res.status(200).send('Hello World!')
-// })
-
-
-// app.get('/users/:userId', function (req, res) {
-//   const params = {
-//     TableName: USERS_TABLE,
-//     Key: {
-//       userId: req.params.userId,
-//     },
-//   }
-
-//   dynamoDb.get(params, (error, result) => {
-//     if (error) {
-//       console.log(error);
-//       res.status(400).json({ error: 'Could not get user' });
-//     }
-//     if (result.Item) {
-//       const {userId, name} = result.Item;
-//       res.json({ userId, name });
-//     } else {
-//       res.status(404).json({ error: "User not found" });
-//     }
-//   });
-// })
-
-// // Create User endpoint
-// app.post('/users', function (req, res) {
-//   const { userId, name } = req.body;
-//   if (typeof userId !== 'string') {
-//     res.status(400).json({ error: '"userId" must be a string' });
-//   } else if (typeof name !== 'string') {
-//     res.status(400).json({ error: '"name" must be a string' });
-//   }
-
-//   const params = {
-//     TableName: USERS_TABLE,
-//     Item: {
-//       userId: userId,
-//       name: name,
-//     },
-//   };
-
-//   dynamoDb.put(params, (error) => {
-//     if (error) {
-//       console.log(error);
-//       res.status(400).json({ error: 'Could not create user' });
-//     }
-//     res.json({ userId, name });
-//   });
-// })
-
-
-//module.exports.server = sls(app)
