fe99d32d035f0d01890d64fe22fd39236650d29b
sunkhanna <sunaina.khanna01@gmail.com>
2018-12-13 13:00:20 +0530

    Update app.js

--- app.js
+++ app.js
@@ -6,91 +6,8 @@ const dynamoDb = new AWS.DynamoDB.DocumentClient();
 const fs = require("fs");
 const join = require("path").join;
 const s3Zip = require("s3-zip");
+var path = require("path");
 var s3 = new AWS.S3();
-
-module.exports.hello = (event, context, callback) => {
-  var response = {
-    statusCode: 200,
-    headers: {
-      "Content-Type": "application/json",
-      "Access-Control-Allow-Origin": "*", // Required for CORS support to work
-      "Access-Control-Allow-Credentials": true // Required for cookies, authorization headers with HTTPS
-    },
-    body: JSON.stringify({
-      message: "my first serverless api"
-    })
-  };
-  callback(null, response);
-};
-
-module.exports.getUsers = function(event, context, callback) {
-  var params = {
-    TableName: USERS_TABLE
-  };
-  dynamoDb.scan(params, function(err, data) {
-    if (err) {
-      var response = {
-        statusCode: 403,
-        headers: {
-          "Content-Type": "application/json",
-          "Access-Control-Allow-Origin": "*", // Required for CORS support to work
-          "Access-Control-Allow-Credentials": true // Required for cookies, authorization headers with HTTPS
-        },
-        body: JSON.stringify(err)
-      };
-      callback(response, null);
-    } else {
-      var response = {
-        statusCode: 200,
-        headers: {
-          "Content-Type": "application/json",
-          "Access-Control-Allow-Origin": "*", // Required for CORS support to work
-          "Access-Control-Allow-Credentials": true // Required for cookies, authorization headers with HTTPS
-        },
-        body: JSON.stringify(data)
-      };
-      callback(null, response);
-    }
-  });
-};
-
-module.exports.createUser = function(event, context, callback) {
-  var json = JSON.parse(event.body);
-  var param = {
-    Item: {
-      userId: json.userId,
-      name: json.name
-    },
-    TableName: USERS_TABLE
-  };
-
-  dynamoDb.put(param, function(err, data) {
-    if (err) {
-      var response = {
-        statusCode: 403,
-        headers: {
-          "Content-Type": "application/json",
-          "Access-Control-Allow-Origin": "*", // Required for CORS support to work
-          "Access-Control-Allow-Credentials": true // Required for cookies, authorization headers with HTTPS
-        },
-        body: JSON.stringify(err)
-      };
-      callback(response, null);
-    } else {
-      console.log("in success code,able to create user", data);
-      var response = {
-        statusCode: 200,
-        headers: {
-          "Content-Type": "application/json",
-          "Access-Control-Allow-Origin": "*", // Required for CORS support to work
-          "Access-Control-Allow-Credentials": true // Required for cookies, authorization headers with HTTPS
-        },
-        body: JSON.stringify(data)
-      };
-      callback(null, response);
-    }
-  });
-};
 
 module.exports.processXmlDataFromS3 = async function(event, context, callback) {
   var options = {
@@ -98,7 +15,7 @@ module.exports.processXmlDataFromS3 = async function(event, context, callback) {
     Key: "xmlsamples.zip"
   };
 
-  var localDestination = path.join(__dirname, options.Key);
+  var localDestination = path.join(__dirname, options.key);
 
   if (typeof localDest == "undefined") {
     localDest = keyName;
@@ -126,17 +43,3 @@ var downloadZipFromS3 = (options, localDestination) => {
     })
     .pipe(file);
 };
-
-// var s = new s3Unzip(
-//   {
-//     bucket:
-//       "https://s3.eu-central-1.amazonaws.com/serverlessnodeapp/xmlsamples.zip",
-//     file: "data.zip",
-//     //deleteOnSuccess: true,
-//     verbose: false
-//   },
-//   function(err, success) {
-//     if (err) console.error(err);
-//     else console.log(success);
-//   }
-// );
