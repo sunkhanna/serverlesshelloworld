609020ef9fa2aa664ce3dc5b70279c122057fd56
sunkhanna <sunaina.khanna01@gmail.com>
2018-12-13 16:46:31 +0530

    Update app.js

--- app.js
+++ app.js
@@ -6,104 +6,97 @@ var AWS = require("aws-sdk");
 const fs = require("fs");
 const join = require("path").join;
 const s3Zip = require("s3-zip");
+
 // var path = require("path");
 // var s3 = new AWS.S3();
 
-module.exports.processXmlDataFromS3 = function() {
-  const region = "eu-central-1";
-  const bucket = "xmltester123";
-  const folder = "xmlzips";
-  const fileName = "xmlsamples.zip";
+// module.exports.processXmlDataFromS3 = function() {
+//   const region = "eu-central-1";
+//   const bucket = "xmltester123";
+//   const folder = "xmlzips";
+//   const fileName = "xmlsamples.zip";
 
-  const output = fs.createWriteStream(join(__dirname, "xml-files.zip"));
+//   const output = fs.createWriteStream(join(__dirname, "xml-files.zip"));
 
-  s3Zip
-    .archive({ region: region, bucket: bucket }, folder, [fileName])
-    .pipe(output);
+//   s3Zip
+//     .archive({ region: region, bucket: bucket }, folder, [fileName])
+//     .pipe(output);
+// };
+
+module.exports.processXmlDataFromS3 = async function(event, context, callback) {
+  console.log("processXmlDataFromS3");
+  // var localDestination = path.join(__dirname, options.key);
+
+  var localDestination = __dirname + "/xmlsamples.zip";
+  if (typeof localDestination == "undefined") {
+    localDestination = keyName;
+  }
+
+  file = fs.createWriteStream(localDestination);
+
+  var response = await downloadZipFromS3(localDestination);
+
+  console.log("response finalllllllll", response);
+
+  var finalResponse = await writeDataToLocalFileSystem(response);
+
+  console.log("final responseeeeeeeeeee", finalResponse);
 };
 
-// module.exports.processXmlDataFromS3 = async function(event, context, callback) {
-//   console.log("processXmlDataFromS3");
-//   // var localDestination = path.join(__dirname, options.key);
+var downloadZipFromS3 = localDestination => {
+  // file = fs.createWriteStream(localDestination);
 
-//   var localDestination = __dirname + "/xmlsamples.zip";
-//   if (typeof localDestination == "undefined") {
-//     localDestination = keyName;
-//   }
+  console.log("download zip");
+  let options = {
+    Bucket: "serverlessnodeapp",
+    Key: "xmlsamples.zip"
+  };
 
-//   //file = fs.createWriteStream(localDestination);
+  console.log("options", options);
 
-//   var response = await downloadZipFromS3(localDestination);
+  console.log("in download zip from s3", localDestination);
 
-//   console.log("response finalllllllll", response);
+  //let file = fs.createWriteStream(localDestination);
 
-//   var finalResponse = await writeDataToLocalFileSystem(response);
+  return new Promise(function(resolve, reject) {
+    console.log("in promise");
+    s3.getObject(options, function(err, data) {
+      console.log("get objectsssssssssssssssss");
+      if (err) {
+        console.log("get objectsssssssssssssssss error", err);
+        reject(err);
+      } else {
+        console.log("get objectsssssssssssssssss data", data);
+        resolve(data);
+      }
+    });
+  });
+  //console.log("in getting object");
+  // Handle any error and exit
+  // if (err) {
+  //   console.log("err", err);
+  //   return err;
+  // } else console.log("data", data);
 
-//   console.log("final responseeeeeeeeeee", finalResponse);
-// };
+  // No error happened
+  // Convert Body from a Buffer to a String
 
-// var downloadZipFromS3 = localDestination => {
-//   file = fs.createWriteStream(localDestination);
-
-//   console.log("download zip");
-//   let options = {
-//     Bucket: "serverlessnodeapp",
-//     Key: "xmlsamples.zip"
-//   };
-
-//   console.log("options", options);
-
-//   console.log("in download zip from s3", localDestination);
-
-//   //let file = fs.createWriteStream(localDestination);
-
-//   return new Promise(function(resolve, reject) {
-//     console.log("in promise");
-//     s3.getObject(options, function(err, data) {
-//       console.log("get objectsssssssssssssssss");
-//       if (err) {
-//         console.log("get objectsssssssssssssssss error", err);
-//         reject(err);
-//       } else {
-//         console.log("get objectsssssssssssssssss data", data);
-//         resolve(data);
-//       }
-//     });
-//   });
-//   //console.log("in getting object");
-//   // Handle any error and exit
-//   // if (err) {
-//   //   console.log("err", err);
-//   //   return err;
-//   // } else console.log("data", data);
-
-//   // No error happened
-//   // Convert Body from a Buffer to a String
-
-//   //let objectData = data.Body.toString('utf-8'); // Use the encoding necessary
-//   // });
-// };
-//   s3.getObject(options)
-//     .createReadStream()
-//     .on("end", () => {
-//       console.log("done");
-//       resolve("succesfully downloaded the file");
-//     })
-//     .on("error", error => {
-//       console.log("not done", error);
-//       reject(error);
-//     })
-//     .pipe(file);
-// };
+  //let objectData = data.Body.toString('utf-8'); // Use the encoding necessary
+  // });
+};
 
 var writeDataToLocalFileSystem = response => {
-  response
-    .createReadStream()
-    .on("end", () => {
-      resolve("seems happened");
-    })
-    .on("error", error => {
-      reject("nopesssss");
-    })
-    .pipe(file);
+  fs.writeFile("/tmp/abc.zip", data.Body, function(err) {
+    if (err) console.log(err.code, "-", err.message);
+    return callback(err);
+  });
+  // response
+  //   .createReadStream()
+  //   .on("end", () => {
+  //     resolve("seems happened");
+  //   })
+  //   .on("error", error => {
+  //     reject("nopesssss");
+  //   })
+  //   .pipe(file);
 };
